2494e51fb537ebd1e6796e998215e572f12ee789
swsong <songaal@gmail.com>
2019-07-31 18:00:24 +0900

    8002 port

--- web.js
+++ web.js
@@ -83,7 +83,7 @@ function main() {
         });
     });
 
-    var server = app.listen(8001, function() {
+    var server = app.listen(8002, function() {
         var host = server.address().address;
         var port = server.address().port;
         console.log('Kakao Chatbot API listening at http://%s:%s', host, port);
